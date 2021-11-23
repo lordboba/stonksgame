7c21afd31f4902ceaf270c20fb5d0cda3a1e597c
Tyler Alex <tyleralex@Charlies-iMac.local>
2021-11-23 10:15:04 -0800

    fixing

--- code/help.py
+++ code/help.py
@@ -16,7 +16,7 @@ async def halp(ctx,set1,set2):
     if set1 == None or set1 == "invite" or set1 == "inv":
         help_emb.add_field(name="Invite",value="$invite, gives a link to invite the bot to other servers.")
     if set1 == "game":
-        game_desc = ["$start [time year=Y month=M day=D], starts a trading game in the channel that lasts for the duration specified or by default 7 days(EX: $start 1M, starts a month-long game)",
+        game_desc = ["$start [time year=Y month=M day=D], starts a trading game in the channel that lasts for the duration specified or by default 7 days(EX: $start 1M, starts a month-long game) Everyone in the game by default starts with $100000",
         "$end, ends the current game playing in the channel. Can only be used by people with the 'stonk-admin' role",
         "$join, allows to join if there is a current game in the channel",
         "$buy [4-Letter Stock Symbol] [Quantity], buys the amount of specified stock(EX: $buy TSLA 50, buys 50 Tesla Stock)",
